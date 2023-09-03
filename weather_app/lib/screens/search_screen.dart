d7d5a01e090631432b0cdca9ba2410aad377bf49
Abdulelah Aljuraysi <72788707+36nv@users.noreply.github.com>
2023-09-03 08:08:51 +0300

    Update search_screen.dart

--- weather_app/lib/screens/search_screen.dart
+++ weather_app/lib/screens/search_screen.dart
@@ -1,85 +1,92 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_typeahead/flutter_typeahead.dart';
-import 'package:http/http.dart' as http;
-import 'package:weather_app/model/weather_model.dart';
 
-import '../Screens/view_page.dart';
-import '../Service/weather_service.dart';
+import '../Service/supabseApi.dart';
+import '../componets/search_appbar_component.dart';
+import '../componets/search_componet.dart';
+import '../componets/weather_card_component.dart';
+import '../model/weather_model.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
-class SearchComponet extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController SearchEdintController = TextEditingController();
+  bool isMorning = false;
+
+  @override
+  void dispose() {
+    SearchEdintController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    TextEditingController city = TextEditingController();
+    return Scaffold(
+      appBar: SearchAppBarComponent(),
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            SearchComponet(),
+            Expanded(
+              child: FutureBuilder(
+                future: getCities(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text('حدث خطأ: ${snapshot.error}'),
+                    );
+                  } else {
+                    List<Weather> cities = snapshot.data!;
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: cities.length,
+                      itemBuilder: (context, index) {
+                        Weather city = cities[index];
+                        var value = city.current!.isDay;
 
-    Future<List<String>> getCitySuggestions(String query) async {
-      final suggestions = <String>[];
-
-      final suggestionUri = Uri.parse(
-        "http://api.weatherapi.com/v1/search.json?key=7788ba5739b1435f9a171734233108&q=" +
-            query,
-      );
-
-      try {
-        final response = await http.get(suggestionUri);
-
-        if (response.statusCode == 200) {
-          final data = jsonDecode(response.body) as List<dynamic>;
-          suggestions.addAll(data.map((item) => item['name'].toString()));
-        }
-      } catch (e) {
-        print('Error fetching city suggestions: $e');
-      }
-      return suggestions;
-    }
-
-    return Column(
-      children: [
-        TypeAheadField<String>(
-          textFieldConfiguration: TextFieldConfiguration(
-            controller: city,
-            decoration: InputDecoration(
-              hintText: 'Enter City',
-              suffixIcon: Container(
-                height: 15,
-                width: 15,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: IconButton(
-                  onPressed: () async {},
-                  icon: Icon(
-                    Icons.search,
-                    color: Colors.black,
-                  ),
-                ),
+                        return Slidable(
+                          actionPane: SlidableDrawerActionPane(),
+                          actionExtentRatio: 0.25,
+                          secondaryActions: [
+                            IconSlideAction(
+                              caption: 'Delete',
+                              color: Colors.red,
+                              icon: Icons.delete,
+                              onTap: () {
+                                removeCity(city.location!.name!);
+                                setState(() {
+                                  cities.removeAt(index);
+                                });
+                              },
+                            ),
+                          ],
+                          child: WeatherCardComponent(
+                            isDay: value!,
+                            cityName: city.location!.name!,
+                            status: city.current!.condition!.text!,
+                            temp: city.current!.tempC.toString(),
+                            weather: cities[index],
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
               ),
             ),
-          ),
-          suggestionsCallback: (pattern) async {
-            return await getCitySuggestions(pattern);
-          },
-          itemBuilder: (context, suggestion) {
-            return ListTile(
-              title: Text(suggestion),
-            );
-          },
-          onSuggestionSelected: (suggestion) async {
-            final Weather weather = await getWeatherData(suggestion);
-            print('Selected suggestion: ${suggestion.runtimeType}');
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => ViewPage(
-                  weather: weather,
-                ),
-              ),
-            );
-          },
+          ],
         ),
-      ],
+      ),
     );
   }
 }
